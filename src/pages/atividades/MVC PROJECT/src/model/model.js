6fec7089319ea765b5d8ee4e8b751f363afe7fde
lissacss <aliciacriss2807@gmail.com>
2023-10-30 20:40:40 -0300

    iugil

--- src/pages/atividades/MVC PROJECT/src/model/model.js
+++ src/pages/atividades/MVC PROJECT/src/model/model.js
@@ -1,4 +1,10 @@
 class Usuario {
+    constructor(){
+        this.nome = "";
+        this.idade = 0;
+        this.login = "";
+        this.senha = "";
+    }
     constructor( nome, idade, login, senha){
         this.nome = nome;
         this.idade = idade;
---
+++ src/pages/atividades/MVC PROJECT/src/view/form-new-user.js
@@ -0,0 +1,13 @@
+const formNewUser ={
+    build :()=>{
+        const root = document.getElementById('root');
+        const formTemplate = document.createElement('form');
+        formTemplate.innerHTML = `
+        <imput type="t"
+        
+        
+        
+        
+        `
+    }
+}
